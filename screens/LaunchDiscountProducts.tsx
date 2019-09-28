1c450c846973859e8a93043f9afaa21e1dd07a3c
Ngoc Vo <duy@koble.com>
2019-09-28 21:04:46 +0300

    Navigate to discount page

--- screens/LaunchDiscountProducts.tsx
+++ screens/LaunchDiscountProducts.tsx
@@ -1,28 +1,32 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { DiscountSettingForm } from '../components/DiscountForm';
+import React from "react";
+import { ScrollView, StyleSheet } from "react-native";
+import { DiscountSettingForm } from "../components/DiscountForm";
 
-export function LaunchDiscountProducts() {
+export function LaunchDiscountProducts(props: any) {
+  const discountedProducts = props.navigation.getParam("products")
+    ? JSON.parse(props.navigation.getParam("products"))
+    : [];
+
   return (
     <ScrollView style={styles.container}>
       {/**
        * Go ahead and delete ExpoLinksView and replace it with your content;
        * we just wanted to provide you with some helpful links.
        */}
-      <DiscountSettingForm />
+      <DiscountSettingForm discountedProducts={discountedProducts} />
     </ScrollView>
   );
 }
 
 LaunchDiscountProducts.navigationOptions = {
-  title: 'Launch product discount',
+  title: "Launch product discount"
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingHorizontal: 15
-  },
+  }
 });
