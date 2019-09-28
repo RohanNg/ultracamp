44832fe7882e5f7e5e81d9ee62b935e5af443f60
Hai Dang Nguyen <hai.dang.nguyen@zalando.fi>
2019-09-28 15:02:05 +0300

    Remove extra shit

--- repositories/index.ts
+++ repositories/index.ts
@@ -9,6 +9,7 @@ export interface ProductItemData {
 
 const PRODUCT_COLLECTION = db.collection('product');
 
+// example getItemById('1').then(i => console.info('item retrieve ', i))
 export async function getItemById(id: string): Promise<ProductItemData> {
     const doc =  await PRODUCT_COLLECTION.doc(id).get()
     return doc.data() as ProductItemData
