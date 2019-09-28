094dc91a2723d14f54439f92080bef2f5b993722
hienminhnguyen717171 <44664474+hienminhnguyen717171@users.noreply.github.com>
2019-09-28 21:25:30 +0300

    Dialog product show detail

--- components/ProductMerchant.tsx
+++ components/ProductMerchant.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 import { Card, Button, Badge, Image } from "react-native-elements";
 import { Product } from "../types/product";
+import Dialog, {
+  DialogTitle,
+  DialogContent,
+  DialogFooter,
+  DialogButton,
+  SlideAnimation,
+  ScaleAnimation,
+} from 'react-native-popup-dialog';
 
 interface ProductMerchantProps {
   product: Product;
@@ -11,8 +19,10 @@ interface ProductMerchantProps {
 }
 export function ProductMerchantComponent(props: ProductMerchantProps) {
   const { product, selected, selectProduct, showButton } = props;
+  let scaleAnimationDialog, defaultAnimationDialog,slideAnimationDialog = false;
 
   return (
+    <View>
     <Card
       containerStyle={{
         padding: 0,
@@ -27,7 +37,9 @@ export function ProductMerchantComponent(props: ProductMerchantProps) {
         style={{ width: "100%", height: 150, borderRadius: 10 }}
       />
       <View style={{ paddingLeft: 10 }}>
-        <Text style={styles.name}>
+        <Text style={styles.name} onPress={() => {
+            scaleAnimationDialog = true
+        }}>
           {product.name}@{product.brand}
         </Text>
         <View style={{ flexDirection: "row", justifyContent: "center" }}>
@@ -54,6 +66,54 @@ export function ProductMerchantComponent(props: ProductMerchantProps) {
         />
       )}
     </Card>
+    
+    <Dialog style={dialogStyle.container}>
+      onTouchOutside={() => {
+        scaleAnimationDialog = false 
+      }}
+      width={0.9}
+      visible={true}
+      dialogAnimation={new ScaleAnimation()}
+      onHardwareBackPress={() => {
+        scaleAnimationDialog= false 
+        return true;
+      }}
+      dialogTitle={
+        <DialogTitle
+          title={product.name}
+          hasTitleBar={false}
+        />
+      }
+      actions={[
+        <DialogButton
+          text="DISMISS"
+          onPress={() => {
+            scaleAnimationDialog= false 
+          }}
+          key="button-1"
+        />,
+      ]}>
+      <DialogContent>
+      <Image
+      source={{ uri: product.imgURL }}
+      style={{ width: 200, height: 200 ,alignItems: 'center' }}
+      />
+        <View style={dialogStyle.container}>
+          <Text>
+            {product.brand}
+          </Text>
+          <Button
+            title="Close"
+            onPress={() => {
+              scaleAnimationDialog= false 
+            }}
+            key="button-1"
+          />
+        </View>
+      </DialogContent>
+    </Dialog>
+   </View>
+    
   );
 }
 
@@ -82,4 +142,13 @@ const styles = StyleSheet.create({
     color: "#C0C0C0",
     marginRight: 5
   }
+});
+
+
+const dialogStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
